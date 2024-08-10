8dc4c10378068d425658a4b2f55e4cd5e86d8ac3
Joao Augusto <joaoaugusto.martis@gmail.com>
2024-08-09 21:56:40 -0300

    saved

--- js/login.js
+++ js/login.js
@@ -1,20 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('loginForm');
-    const usernameInput = document.getElementById('username');
-    const passwordInput = document.getElementById('password');
+document.addEventListener('DOMContentLoaded', () => {
+    // Obtém os elementos do formulário e do botão
+    const loginForm = document.getElementById('loginForm');
+    const entrarButton = document.querySelector('.btn-entrar');
+    const errorMessage = document.getElementById('error-message');
+    
+    // Adiciona um ouvinte de eventos para o clique no botão "Entrar"
+    entrarButton.addEventListener('click', (event) => {
+        event.preventDefault(); // Previne o comportamento padrão do link
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault(); // Impede o envio padrão do formulário
+        // Obtém os valores dos campos do formulário
+        const userType = document.getElementById('userType').value;
+        const password = document.getElementById('password').value;
 
-        const username = usernameInput.value.trim();
-        const password = passwordInput.value.trim();
-
-        if ((username === 'GERENCIAL' || username === 'FUNCIONARIO') && password === '123') {
-            // Se as credenciais estiverem corretas, redirecionar para a página do calendário
+        // Verifica se o campo "Tipo de Usuário" foi preenchido e se a senha foi informada
+        if (userType && password) {
+            // Se o formulário for válido, redireciona para a página de calendário
             window.location.href = 'calendario.html';
         } else {
-            // Se as credenciais estiverem incorretas, exibir uma mensagem de erro
-            alert('Usuário ou senha incorretos.');
+            // Se algum campo não for preenchido, mostra uma mensagem de erro
+            errorMessage.textContent = 'Por favor, preencha todos os campos corretamente.';
+            errorMessage.classList.add('show');
+
+            // Oculta a mensagem após alguns segundos
+            setTimeout(() => {
+                errorMessage.classList.remove('show');
+            }, 3000); // 3 segundos
         }
     });
 });
--- js/reservas.js
+++ js/reservas.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Funções e variáveis do formulário de clientes
     const clientForm = document.getElementById("clientForm");
     const clientsTableBody = document.getElementById("clientsTableBody");
     const clientInput = document.getElementById("cliente");
@@ -156,4 +157,54 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     renderClients();
+
+    // Funções e variáveis da dropzone
+    const dropzoneBox = document.getElementsByClassName("dropzone-box")[0];
+    const inputFiles = document.querySelectorAll(".dropzone-area input[type='file']");
+    const inputElement = inputFiles[0];
+    const dropZoneElement = inputElement.closest(".dropzone-area");
+
+    inputElement.addEventListener("change", (e) => {
+        if (inputElement.files.length) {
+            updateDropzoneFileList(dropZoneElement, inputElement.files[0]);
+        }
+    });
+
+    dropZoneElement.addEventListener("dragover", (e) => {
+        e.preventDefault();
+        dropZoneElement.classList.add("dropzone--over");
+    });
+
+    ["dragleave", "dragend"].forEach((type) => {
+        dropZoneElement.addEventListener(type, (e) => {
+            dropZoneElement.classList.remove("dropzone--over");
+        });
+    });
+
+    dropZoneElement.addEventListener("drop", (e) => {
+        e.preventDefault();
+        if (e.dataTransfer.files.length) {
+            inputElement.files = e.dataTransfer.files;
+            updateDropzoneFileList(dropZoneElement, e.dataTransfer.files[0]);
+        }
+        dropZoneElement.classList.remove("dropzone--over");
+    });
+
+    const updateDropzoneFileList = (dropzoneElement, file) => {
+        let dropzoneFileMessage = dropzoneElement.querySelector(".file-info");
+        dropzoneFileMessage.innerHTML = `
+            ${file.name}, ${file.size} bytes
+        `;
+    };
+
+    dropzoneBox.addEventListener("reset", (e) => {
+        let dropzoneFileMessage = dropZoneElement.querySelector(".file-info");
+        dropzoneFileMessage.innerHTML = `Nenhum Arquivo Selecionado`;
+    });
+
+    dropzoneBox.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const myFile = document.getElementById("upload-file");
+        console.log(myFile.files[0]);
+    });
 });
